32aab2cc3d17f1a6b6c96002f9a8e57eb9804130
0sasha2 <volshebnik2014@gmail.com>
2025-02-10 18:44:38 -0600

    edits

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Software Voxci
 Repo for the application written in python
+
+Instalation instruction for windows:
+ctr+shift+p to start command pallet 
+python: create an enviroment 
+venv
--- main.py
+++ main.py
@@ -1,17 +1,34 @@
-from typing import Union
+from typing import BaseModel
 from fastapi import FastAPI
 
 app = FastAPI()
 
+class CreateDieInput(BaseModel):
+    id: str
+    name: str
 
+
+#Fetches die serial numbers and names
 @app.get("/dies")
 def get_dies():
     return {"Hello": "World"}
 
+#Fetches data sweeps for the die
 @app.get("/dies/{die_id}")
-def get_die():
+def get_die(die_id: str):
     return {"Hello": "World"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+#Creates a new die 
+@app.post("/dies")
+def create_die(die: CreateDieInput):
+    return {"id": die.id}
+
+#Adds a sweep to existing die
+@app.post("/dies/{die_id}/sweeps")
+def add_die_sweep(die_id: str):
+    return {"Hello": "World"}
+
+#Deletes all sweeps
+@app.delete("/dies/{die_id}")
+def delete_die(die_id: str):
+    return {"Hello": "World"}
