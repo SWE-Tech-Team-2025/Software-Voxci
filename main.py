399b92f9137ce07d66a4244dc6f7463fb2881c89
samantha <sraby4723@proton.me>
2025-02-12 18:33:06 -0600

    Added code to make requests to and from the database

--- main.py
+++ main.py
@@ -1,34 +1,44 @@
-from typing import BaseModel
+import os
+from pydantic import BaseModel
 from fastapi import FastAPI
+from pymongo import MongoClient
+from dotenv import load_dotenv
 
-app = FastAPI()
+load_dotenv()
+# app = FastAPI()
+client = MongoClient(os.getenv("MONGODB_URI"))
+db = client["data"]
+collection = db["dies"]
+# print(collection.insert_one({"name" : "Samantha"}))
+
+for die in collection.find({"name" : "Samantha"}):
+    print(die)
 
 class CreateDieInput(BaseModel):
     id: str
     name: str
 
+# #Fetches die serial numbers and names
+# @app.get("/dies")
+# def get_dies():
+#     return {"Hello": "World"}
 
-#Fetches die serial numbers and names
-@app.get("/dies")
-def get_dies():
-    return {"Hello": "World"}
+# #Fetches data sweeps for the die
+# @app.get("/dies/{die_id}")
+# def get_die(die_id: str):
+#     return {"Hello": "World"}
 
-#Fetches data sweeps for the die
-@app.get("/dies/{die_id}")
-def get_die(die_id: str):
-    return {"Hello": "World"}
+# #Creates a new die 
+# @app.post("/dies")
+# def create_die(die: CreateDieInput):
+#     return {"id": die.id}
 
-#Creates a new die 
-@app.post("/dies")
-def create_die(die: CreateDieInput):
-    return {"id": die.id}
+# #Adds a sweep to existing die
+# @app.post("/dies/{die_id}/sweeps")
+# def add_die_sweep(die_id: str):
+#     return {"Hello": "World"}
 
-#Adds a sweep to existing die
-@app.post("/dies/{die_id}/sweeps")
-def add_die_sweep(die_id: str):
-    return {"Hello": "World"}
-
-#Deletes all sweeps
-@app.delete("/dies/{die_id}")
-def delete_die(die_id: str):
-    return {"Hello": "World"}
+# #Deletes all sweeps
+# @app.delete("/dies/{die_id}")
+# def delete_die(die_id: str):
+#     return {"Hello": "World"}
