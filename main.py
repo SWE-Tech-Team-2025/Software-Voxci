9e939c8e1a8ada82fa1671a44f3aa4329e76d36a
Samantha <sraby4723@proton.me>
2025-02-16 15:45:05 -0600

    Added python file for the excel exporting and pasted example code for ESP32 communication

--- main.py
+++ main.py
@@ -27,7 +27,30 @@ class CreateDieInput(BaseModel):
 # @app.get("/dies/{die_id}")
 # def get_die(die_id: str):
 #     return {"Hello": "World"}
+# #Fetches die serial numbers and names
+# @app.get("/dies")
+# def get_dies():
+#     return {"Hello": "World"}
 
+# #Fetches data sweeps for the die
+# @app.get("/dies/{die_id}")
+# def get_die(die_id: str):
+#     return {"Hello": "World"}
+
+# #Creates a new die 
+# @app.post("/dies")
+# def create_die(die: CreateDieInput):
+#     return {"id": die.id}
+
+# #Adds a sweep to existing die
+# @app.post("/dies/{die_id}/sweeps")
+# def add_die_sweep(die_id: str):
+#     return {"Hello": "World"}
+
+# #Deletes all sweeps
+# @app.delete("/dies/{die_id}")
+# def delete_die(die_id: str):
+#     return {"Hello": "World"}
 # #Creates a new die 
 # @app.post("/dies")
 # def create_die(die: CreateDieInput):
