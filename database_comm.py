7fb781efcfa86c6ce7cc4bb4444e5700b2b24e15
Samantha <sraby4723@proton.me>
2025-02-23 16:03:16 -0600

    Separated database communication from main

---
+++ database_comm.py
@@ -0,0 +1,61 @@
+load_dotenv()
+app = FastAPI()
+client = MongoClient(os.getenv("MONGODB_URI"))
+db = client["data"]
+collection = db["dies"]
+# print(collection.insert_one({"name" : "Samantha"}))
+
+for die in collection.find({"name" : "Samantha"}):
+    print(die)
+
+class CreateDieInput(BaseModel):
+    id: str
+    name: str
+
+#Fetches die serial numbers and names
+@app.get("/dies")
+def get_dies():
+    return {"Hello": "World"}
+
+#Fetches data sweeps for the die
+@app.get("/dies/{die_id}")
+def get_die(die_id: str):
+    return {"Hello": "World"}
+#Fetches die serial numbers and names
+@app.get("/dies")
+def get_dies():
+    return {"Hello": "World"}
+
+#Fetches data sweeps for the die
+@app.get("/dies/{die_id}")
+def get_die(die_id: str):
+    return {"Hello": "World"}
+
+#Creates a new die 
+@app.post("/dies")
+def create_die(die: CreateDieInput):
+    return {"id": die.id}
+
+#Adds a sweep to existing die
+@app.post("/dies/{die_id}/sweeps")
+def add_die_sweep(die_id: str):
+    return {"Hello": "World"}
+
+#Deletes all sweeps
+@app.delete("/dies/{die_id}")
+def delete_die(die_id: str):
+    return {"Hello": "World"}
+#Creates a new die 
+@app.post("/dies")
+def create_die(die: CreateDieInput):
+    return {"id": die.id}
+
+#Adds a sweep to existing die
+@app.post("/dies/{die_id}/sweeps")
+def add_die_sweep(die_id: str):
+    return {"Hello": "World"}
+
+#Deletes all sweeps
+@app.delete("/dies/{die_id}")
+def delete_die(die_id: str):
+    return {"Hello": "World"}
