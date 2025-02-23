65b3e1900d859bf7f7277849cb4102e32b5e4496
Samantha <sraby4723@proton.me>
2025-02-23 16:01:14 -0600

    Started code for the communicator and separated stuff out a bit more into their own files

--- main.py
+++ main.py
@@ -3,9 +3,15 @@ from pydantic import BaseModel
 from fastapi import FastAPI
 from pymongo import MongoClient
 from dotenv import load_dotenv
+from WiFiCommunicator import WiFiCommunicator
+
+
+# Global Variables
+int curr_chip_id = 0
+ communicator = WiFiCommunicator(max_buffer_sz=256)
 
 load_dotenv()
-# app = FastAPI()
+app = FastAPI()
 client = MongoClient(os.getenv("MONGODB_URI"))
 db = client["data"]
 collection = db["dies"]
@@ -18,50 +24,50 @@ class CreateDieInput(BaseModel):
     id: str
     name: str
 
-# #Fetches die serial numbers and names
-# @app.get("/dies")
-# def get_dies():
-#     return {"Hello": "World"}
+#Fetches die serial numbers and names
+@app.get("/dies")
+def get_dies():
+    return {"Hello": "World"}
 
-# #Fetches data sweeps for the die
-# @app.get("/dies/{die_id}")
-# def get_die(die_id: str):
-#     return {"Hello": "World"}
-# #Fetches die serial numbers and names
-# @app.get("/dies")
-# def get_dies():
-#     return {"Hello": "World"}
+#Fetches data sweeps for the die
+@app.get("/dies/{die_id}")
+def get_die(die_id: str):
+    return {"Hello": "World"}
+#Fetches die serial numbers and names
+@app.get("/dies")
+def get_dies():
+    return {"Hello": "World"}
 
-# #Fetches data sweeps for the die
-# @app.get("/dies/{die_id}")
-# def get_die(die_id: str):
-#     return {"Hello": "World"}
+#Fetches data sweeps for the die
+@app.get("/dies/{die_id}")
+def get_die(die_id: str):
+    return {"Hello": "World"}
 
-# #Creates a new die 
-# @app.post("/dies")
-# def create_die(die: CreateDieInput):
-#     return {"id": die.id}
+#Creates a new die 
+@app.post("/dies")
+def create_die(die: CreateDieInput):
+    return {"id": die.id}
 
-# #Adds a sweep to existing die
-# @app.post("/dies/{die_id}/sweeps")
-# def add_die_sweep(die_id: str):
-#     return {"Hello": "World"}
+#Adds a sweep to existing die
+@app.post("/dies/{die_id}/sweeps")
+def add_die_sweep(die_id: str):
+    return {"Hello": "World"}
 
-# #Deletes all sweeps
-# @app.delete("/dies/{die_id}")
-# def delete_die(die_id: str):
-#     return {"Hello": "World"}
-# #Creates a new die 
-# @app.post("/dies")
-# def create_die(die: CreateDieInput):
-#     return {"id": die.id}
+#Deletes all sweeps
+@app.delete("/dies/{die_id}")
+def delete_die(die_id: str):
+    return {"Hello": "World"}
+#Creates a new die 
+@app.post("/dies")
+def create_die(die: CreateDieInput):
+    return {"id": die.id}
 
-# #Adds a sweep to existing die
-# @app.post("/dies/{die_id}/sweeps")
-# def add_die_sweep(die_id: str):
-#     return {"Hello": "World"}
+#Adds a sweep to existing die
+@app.post("/dies/{die_id}/sweeps")
+def add_die_sweep(die_id: str):
+    return {"Hello": "World"}
 
-# #Deletes all sweeps
-# @app.delete("/dies/{die_id}")
-# def delete_die(die_id: str):
-#     return {"Hello": "World"}
+#Deletes all sweeps
+@app.delete("/dies/{die_id}")
+def delete_die(die_id: str):
+    return {"Hello": "World"}
