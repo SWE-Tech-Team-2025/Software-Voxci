8382a19fea932162f50b067c217bf00869f801cb
aradhana choudhury <aradhanac@aradhanas-MacBook-Air.local>
2025-02-28 17:12:16 -0600

    Started implementation of the exporter

--- excel_export.py
+++ excel_export.py
@@ -1,18 +1,23 @@
-import database_comm
+# import database_comm
 import xlsxwriter
 
-class Exporter:
+workbook = xlsxwriter.Workbook('hello.xlsx')
+worksheet = workbook.add_worksheet()
+worksheet.write('A1', 'Hello world')
+workbook.close()
 
-    '''
-    Class definition for the Excel Exporter. 
-    TODO: Complete code based off example code and complete
-    based on our use case. 
-    '''
+# class Exporter:
+    
+#     '''
+#     Class definition for the Excel Exporter. 
+#     TODO: Complete code based off example code and complete
+#     based on our use case. 
+#     '''
 
-    '''
-    TODO: Finish initializer to get all the variables and/or communicators
-    to get data from the database from a specific die
-    '''
-    def __init__(self, chip_id: int) -> None:
-        self.chip_id = chip_id
-        workbook = xlsxwriter.Workbook('hello.xlsx')
+#     '''
+#     TODO: Finish initializer to get all the variables and/or communicators
+#     to get data from the database from a specific die
+#     '''
+
+#     def __init__(self, chip_id: int) -> None:
+#         self.chip_id = chip_id
