5775006f3901ff9c711fb4fc14a99944dbbc096b
Samantha <sraby4723@proton.me>
2025-03-02 15:02:21 -0600

    Uncommented source code

--- excel_export.py
+++ excel_export.py
@@ -6,18 +6,18 @@ worksheet = workbook.add_worksheet()
 worksheet.write('A1', 'Hello world')
 workbook.close()
 
-# class Exporter:
+class Exporter:
     
-#     '''
-#     Class definition for the Excel Exporter. 
-#     TODO: Complete code based off example code and complete
-#     based on our use case. 
-#     '''
+    '''
+    Class definition for the Excel Exporter. 
+    TODO: Complete code based off example code and complete
+    based on our use case. 
+    '''
 
-#     '''
-#     TODO: Finish initializer to get all the variables and/or communicators
-#     to get data from the database from a specific die
-#     '''
+    '''
+    TODO: Finish initializer to get all the variables and/or communicators
+    to get data from the database from a specific die
+    '''
 
-#     def __init__(self, chip_id: int) -> None:
-#         self.chip_id = chip_id
+    def __init__(self, chip_id: int) -> None:
+        self.chip_id = chip_id
