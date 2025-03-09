787bca2c9dbee3e025a95ea9e11aaac1672ee9ce
Samantha <sraby4723@proton.me>
2025-03-09 16:19:44 -0500

    Changes to the main for Python

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from esp32_wifi_communicator import WiFiCommunicator
 from database_comm import DataBaseComm
+import uuid
 
-def main():
-    curr_chip_id = 0
+class Main:
+    curr_chip_id = "empty"
     communicator = WiFiCommunicator(max_buffer_sz=128)
     database_comm = DataBaseComm()
+    def new_die_test()-> str:
+        curr_chip_id = uuid4()
+        database_comm.create_die()
+        
+        return curr_chip_id
 
-def run():
-    
+    def main():
+        return
+
+    def run():
+        return       
