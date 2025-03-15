e68e404d42c7504ac5d0ef17810ca8ad8993a24e
aradhana choudhury <aradhanac@aradhanas-MacBook-Air.local>
2025-03-15 15:51:02 -0500

    wrote a function that takes data from the database and writes the excel file

--- excel_export.py
+++ excel_export.py
@@ -1,4 +1,4 @@
-# import database_comm
+import database_comm
 import xlsxwriter
 
 class Exporter:
@@ -13,30 +13,39 @@ class Exporter:
     TODO: Finish initializer to get all the variables and/or communicators
     to get data from the database from a specific die
     '''
-    def __init__(self, chip_id: int) -> None:
-        self.chip_id = chip_id
-        workbook = xlsxwriter.Workbook('hello.xlsx')
+    # def __init__(self, chip_id: int) -> None:
+    #     self.chip_id = chip_id
+    #     workbook = xlsxwriter.Workbook('hello.xlsx')
 
-workbook = xlsxwriter.Workbook('test.xlsx')
-worksheet = workbook.add_worksheet() # can name the worksheet
+    def writeData(self, name, chip_id: str):
 
-# data to write to worksheet
-data = (
-    ['Time', 1000],
-    ['Voltage', 127],
-    ['Calculated Capacitance', 300],
-    ['Humidity', 50],
-    ['Temp', 25],
-)
+        workbook = xlsxwriter.Workbook(name + '.xlsx')
+        worksheet = workbook.add_worksheet() # can name the worksheet
 
-row = 0
-col = 0
+        # get data from database_comm
+        time = None
+        voltage = None
+        calculated_capcitance = None
+        humidity = None
+        temp = None
 
-# iterate over data and write it out row by row
-for variable, data in (data):
-    # worksheet.write(row, col, some_data)
-    worksheet.write(row, col, variable)
-    worksheet.write(row, col + 1, data)
-    row += 1
+        # data to write to worksheet
+        data = (
+            ['Time', 1000], # replace with variable
+            ['Voltage', 127],
+            ['Calculated Capacitance', 300],
+            ['Humidity', 50],
+            ['Temp', 25],
+        )
 
-workbook.close()
+        row = 0
+        col = 0
+
+        # iterate over data and write it out row by row
+        for variable, data in (data):
+            # worksheet.write(row, col, some_data)
+            worksheet.write(row, col, variable)
+            worksheet.write(row, col + 1, data)
+            row += 1
+
+        workbook.close()
