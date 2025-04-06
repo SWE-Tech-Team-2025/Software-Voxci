cc13a63bc67b4850f75a89d573af236f2ca33457
aradhana choudhury <aradhanac@aradhanas-MacBook-Air.local>
2025-04-06 15:45:21 -0500

    indexing for sweeps

--- excel_export.py
+++ excel_export.py
@@ -20,35 +20,38 @@ class Exporter:
 
     def write_data(self, name, chip_id: str, test_num: int):
 
-        workbook = xlsxwriter.Workbook(name + '.xlsx')
+        workbook = xlsxwriter.Workbook(name + '.xlsx') 
         worksheet = workbook.add_worksheet() # can name the worksheet
 
-        sweeps = get_die_sweeps(chip_id)
+        sweeps = get_die_sweeps(chip_id, test_num)
 
-        # get data from database_comm
-        time = None
-        voltage = None
-        calculated_capcitance = None
-        humidity = None
-        temp = None
+        for sweep in (sweeps):
 
-        # data to write to worksheet
-        data = (
-            ['Time', 1000], # replace with variable
-            ['Voltage', 127],
-            ['Calculated Capacitance', 300],
-            ['Humidity', 50],
-            ['Temp', 25],
-        )
+            # get data from database_comm
+            time = sweep[2]
+            humidity = sweep[3]
+            voltage = sweep[4]
+            capacitance = sweep[5]
+            temp = sweep[6]
 
-        row = 0
-        col = 0
+            # data to write to worksheet
+            data = (
+                ['Time', time], # replace with variable
+                ['Humidity', humidity],
+                ['Voltage', voltage],
+                ['Calculated Capacitance', capacitance],
+                ['Temp', temp],
+            )
 
-        # iterate over data and write it out row by row
-        for variable, data in (data):
-            # worksheet.write(row, col, some_data)
-            worksheet.write(row, col, variable)
-            worksheet.write(row, col + 1, data)
-            row += 1
+            row = 0
+            col = 0
+
+            # iterate over data and write it out row by row
+            for variable, data in (data):
+                # worksheet.write(row, col, some_data)
+                worksheet.write(row, col, variable)
+                worksheet.write(row, col + 1, data)
+                row += 1
+
 
         workbook.close()
