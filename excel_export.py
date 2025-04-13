20e817f027d906ef38aed94e3dfacd3a447f3852
aradhana choudhury <aradhanac@aradhanas-MacBook-Air.local>
2025-04-13 15:35:13 -0500

    modified write_data to write a spreadsheet based off wanted test number

--- excel_export.py
+++ excel_export.py
@@ -18,43 +18,87 @@ class Exporter:
     #     self.chip_id = chip_id
     #     workbook = xlsxwriter.Workbook('hello.xlsx')
 
-    def write_data(self, name, chip_id: str, test_num: int):
+    def write_data(self, name, chip_id: str, test_num: int): # test_num -1 if all tests requested
 
-        workbook = xlsxwriter.Workbook(name + '.xlsx') 
-        worksheet = workbook.add_worksheet() # can name the worksheet
+        workbook = xlsxwriter.Workbook(name + '.xlsx')  
 
-        sweeps = get_die_sweeps(chip_id, test_num)
+        if test_num == -1: # if all tests are wanted
+            for i in range(test_num):
 
-        worksheet.write('A1', 'Chip ID: ' + chip_id)
-        worksheet.write('B1', 'Test Number' + test_num)
+                worksheet = workbook.add_worksheet(test_num) # worksheet is named by test number
 
-        for sweep in (sweeps):
+                sweeps = get_die_sweeps(chip_id, test_num)
 
-            # get data from database_comm
-            time = sweep[2]
-            humidity = sweep[3]
-            voltage = sweep[4]
-            capacitance = sweep[5]
-            temp = sweep[6]
+                worksheet.write('A1', 'Chip ID: ' + chip_id)
+                worksheet.write('B1', 'Test Number' + test_num)
 
-            # data to write to worksheet
-            data = (
-                ['Time', time],
-                ['Humidity', humidity],
-                ['Voltage', voltage],
-                ['Calculated Capacitance', capacitance],
-                ['Temp', temp],
-            )
+                for sweep in (sweeps):
 
-            row = 1
-            col = 0
+                   # get data from database_comm
+                    time = sweep[2]
+                    humidity = sweep[3]
+                    voltage = sweep[4]
+                    capacitance = sweep[5]
+                    temp = sweep[6]
 
-            # iterate over data and write it out row by row
-            for variable, data in (data):
-                # worksheet.write(row, col, some_data)
-                worksheet.write(row, col, variable)
-                worksheet.write(row, col + 1, data)
-                row += 1
+                    # data to write to worksheet
+                    data = (
+                        ['Time', time],
+                        ['Humidity', humidity],
+                        ['Voltage', voltage],
+                        ['Calculated Capacitance', capacitance],
+                        ['Temp', temp],
+                    )
 
+                    row = 1
+                    col = 0
 
-        workbook.close()
+                    # iterate over data and write it out row by row
+                    for variable, data in (data):
+                        # worksheet.write(row, col, some_data)
+                        worksheet.write(row, col, variable)
+                        worksheet.write(row, col + 1, data)
+                        row += 1
+
+                test_num += 1 #increase the test number to look at the next test
+            
+            workbook.close()
+
+        else: # if only one specific test is wanted
+
+            worksheet = workbook.add_worksheet() # can name the worksheet
+
+            sweeps = get_die_sweeps(chip_id, test_num)
+
+            worksheet.write('A1', 'Chip ID: ' + chip_id)
+            worksheet.write('B1', 'Test Number' + test_num)
+
+            for sweep in (sweeps):
+
+                   # get data from database_comm
+                    time = sweep[2]
+                    humidity = sweep[3]
+                    voltage = sweep[4]
+                    capacitance = sweep[5]
+                    temp = sweep[6]
+
+                    # data to write to worksheet
+                    data = (
+                        ['Time', time],
+                        ['Humidity', humidity],
+                        ['Voltage', voltage],
+                        ['Calculated Capacitance', capacitance],
+                        ['Temp', temp],
+                    )
+
+                    row = 1
+                    col = 0
+
+                    # iterate over data and write it out row by row
+                    for variable, data in (data):
+                        # worksheet.write(row, col, some_data)
+                        worksheet.write(row, col, variable)
+                        worksheet.write(row, col + 1, data)
+                        row += 1
+                
+            workbook.close()
