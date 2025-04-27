2c40204872b7d6913c84d3663fc76c13a34563c3
aradhana choudhury <aradhanac@aradhanas-MacBook-Air.local>
2025-04-27 16:16:00 -0500

    added test code

--- tester.py
+++ tester.py
@@ -1,6 +1,7 @@
 from esp32_wifi_communicator import WiFiCommunicator
 from database_comm import DataBaseComm
 import uuid
+import time
 
 # Tester file to ensure proper functionality of all code
 
@@ -8,3 +9,8 @@ import uuid
 
 def main() -> None:
     test_esp_comm = WiFiCommunicator(128, 11111)
+    test_esp_comm.send_message("test")
+    time.sleep(0.1)
+    out_message = test_esp_comm.get_message()
+    if out_message == None:
+        print("failed")
