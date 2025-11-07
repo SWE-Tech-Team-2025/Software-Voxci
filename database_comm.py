9e881a7936e909d1a9f9fa9e4e9d2230bf8b50a5
Samantha Raby <sraby4723@proton.me>
2025-11-07 17:05:16 -0600

    Starting implementation for the communication throughout the app via FastAPI

--- database_comm.py
+++ database_comm.py
@@ -1,17 +1,9 @@
 import os
 from pydantic import BaseModel
-from fastapi import FastAPI
 from pymongo import MongoClient
 from dotenv import load_dotenv
 from datetime import datetime
-from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel
-from pymongo import MongoClient
-import jwt
-from jwt import encode as jwt_encode
-from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
-from fastapi import Depends, HTTPException
-from bson import ObjectId
+
 
 class DataBaseComm:
 
@@ -50,13 +42,13 @@ class DataBaseComm:
     # Increments testnum for a specific die
     def increment_die_testnum(self, die_id : str) -> None:
         die_query = {"ID" : die_id}
-        die_new_test_val = {"inc" : {"currtestnum"}}
+        die_new_test_val = {"inc" : {"currtestnum" : 1}}
         dies.update_one(die_query, die_new_test_val)
 
     # Returns the specified die's testnum for use in generating sweeps
     def get_die_testnum(self, die_id : str) -> int:
-        die = dies.find({"ID" : die_id})
-        return die[1]
+        die = dies.find_one({"ID" : die_id})
+        return die["currtestnum"] if die else None
 
     # Fetches all sweeps from the database that correspond to the die id
     def get_die_sweeps(self, die_id, test_num) -> list:
@@ -69,62 +61,3 @@ class DataBaseComm:
     def create_die_sweep(self, die_id : str, test_num : int, sweep_voltage : float, sweep_humidity : float, sweep_temp : float, sweep_capacitance : float) -> None:
         new_sweep = { "ID" : die_id, "testnum" : test_num , "timestamp" : datetime.now(), "humidity" : sweep_humidity, "voltage" : sweep_voltage, "capacitance" : sweep_capacitance, "temp" : sweep_temp}
         die_sweeps.insert_one(new_sweep)
-
-
-# class CreateDieInput(BaseModel):
-#     id: str
-#     name: str
-
-#     # Fetches die serial numbers and names
-#     @app.get("/dies")
-#     def get_dies():
-#         return {"Hello": "World"}
-
-#     # Fetches data sweeps for the die
-#     @app.get("/dies/{die_id}")
-#     def get_die(die_id: str):
-#         dies = db["dies"]
-#         for die in dies.find({"name" : die_id}):
-#             print(die)
-#         return {"Hello": "World"}
-
-#     # Fetches die serial numbers and names
-#     @app.get("/dies")
-#     def get_dies():
-#         return {"Hello": "World"}
-
-#     # Fetches data sweeps for the die
-#     @app.get("/dies/{die_id}")
-#     def get_die(die_id: str):
-#         return {"Hello": "World"}
-
-#     # Creates a new die 
-#     @app.post("/dies")
-#     def create_die(die: CreateDieInput):
-#         return {"id": die.id}
-
-#     # Adds a sweep to existing die
-#     @app.post("/dies/{die_id}/sweeps")
-#     def add_die_sweep(die_id: str, sweep: float):
-#         die_sweeps.insert_one(die_id, sweep)
-#         return {"Hello": "World"}
-
-#     # Deletes all sweeps
-#     @app.delete("/dies/{die_id}")
-#     def delete_die(die_id: str):
-#         return {"Hello": "World"}
-
-#     # Creates a new die 
-#     @app.post("/dies")
-#     def create_die(die: CreateDieInput):
-#         return {"id": die.id}
-
-#     # Adds a sweep to existing die
-#     @app.post("/dies/{die_id}/sweeps")
-#     def add_die_sweep(die_id: str):
-#         return {"Hello": "World"}
-
-#     # Deletes all sweeps
-#     @app.delete("/dies/{die_id}")
-#     def delete_die(die_id: str):
-#         return {"Hello": "World"}
--- main.py
+++ main.py
@@ -2,6 +2,13 @@ from esp32_wifi_communicator import WiFiCommunicator
 from database_comm import DataBaseComm
 from excel_export import Exporter
 import uuid
+from fastapi import FastAPI
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from fastapi import Depends, HTTPException
+from bson import ObjectId
+from fastapi.middleware.cors import CORSMiddleware
+import jwt
+from jwt import encode as jwt_encode
 
 class Main:
     curr_chip_id = "empty"
@@ -21,7 +28,68 @@ class Main:
     
 
     def main():
+
         return
 
     def run():
+        
+        main()
         return       
+
+# class CreateDieInput(BaseModel):
+#     id: str
+#     name: str
+
+#     # Fetches die serial numbers and names
+#     @app.get("/dies")
+#     def get_dies():
+#         return {"Hello": "World"}
+
+#     # Fetches data sweeps for the die
+#     @app.get("/dies/{die_id}")
+#     def get_die(die_id: str):
+#         dies = db["dies"]
+#         for die in dies.find({"name" : die_id}):
+#             print(die)
+#         return {"Hello": "World"}
+
+#     # Fetches die serial numbers and names
+#     @app.get("/dies")
+#     def get_dies():
+#         return {"Hello": "World"}
+
+#     # Fetches data sweeps for the die
+#     @app.get("/dies/{die_id}")
+#     def get_die(die_id: str):
+#         return {"Hello": "World"}
+
+#     # Creates a new die 
+#     @app.post("/dies")
+#     def create_die(die: CreateDieInput):
+#         return {"id": die.id}
+
+#     # Adds a sweep to existing die
+#     @app.post("/dies/{die_id}/sweeps")
+#     def add_die_sweep(die_id: str, sweep: float):
+#         die_sweeps.insert_one(die_id, sweep)
+#         return {"Hello": "World"}
+
+#     # Deletes all sweeps
+#     @app.delete("/dies/{die_id}")
+#     def delete_die(die_id: str):
+#         return {"Hello": "World"}
+
+#     # Creates a new die 
+#     @app.post("/dies")
+#     def create_die(die: CreateDieInput):
+#         return {"id": die.id}
+
+#     # Adds a sweep to existing die
+#     @app.post("/dies/{die_id}/sweeps")
+#     def add_die_sweep(die_id: str):
+#         return {"Hello": "World"}
+
+#     # Deletes all sweeps
+#     @app.delete("/dies/{die_id}")
+#     def delete_die(die_id: str):
+#         return {"Hello": "World"}
